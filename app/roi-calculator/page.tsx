20d8e30258e0a85927fedad6abad34ea3a98a42b
Anirban Ghosh <anirbanghosh060@gmail.com>
2024-11-24 22:42:22 +0530

    rest for now

--- app/roi-calculator/page.tsx
+++ app/roi-calculator/page.tsx
@@ -1,10 +1,10 @@
 "use client"
-import RoiCalculator from "@/components/roi-calculator"
+import RoiCalculator from "../components/roi-calculator"
 
 export default function Page() {
   return (
     <div className="container py-10">
-      <RoiCalculator schema={undefined} defaultValues={undefined} steps={[]} onSubmit={async function (): Promise<void> {
+      <RoiCalculator schema={undefined} defaultValues={undefined} steps={[]} onSubmit={function (values: any): Promise<void> {
         throw new Error("Function not implemented.")
       } } />
     </div>
